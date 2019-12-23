b23bd1c828865accd1be087e45c09ed1d3377114
daniel <daniel.gil.saenz.trabajo@gmail.com>
2019-12-23 14:32:57 +0100

    He hecho algo raro al hacer commit, y creo que he hecho un merge sin querer. Esta todo bien

--- PongCliente_Sockets_GIT/PongCliente_Sockets/Program.cs
+++ PongCliente_Sockets_GIT/PongCliente_Sockets/Program.cs
@@ -135,7 +135,7 @@ namespace PongCliente_Sockets
                 );
 
             // Initialize the Ball
-            ball = new Ball(new fPoint(screenHandler.max_W / 2, screenHandler.max_H / 2),new fVector(2,4));
+            ball = new Ball(new fPoint(screenHandler.max_W / 2, screenHandler.max_H / 2),new fVector(2,5));
 
             // Initialize the scoreBoard
             statusBoard = new StatusBoard(new Point(screenHandler.max_W / 2, 3),0, 0, 10);
