6383c8bc7cad998ae194f02fc7ac94176cfdcc82
daniel <daniel.gil.saenz.trabajo@gmail.com>
2020-01-13 16:46:09 +0100

    realizado el hello world

--- PongCliente_Sockets_GIT/PruebaServidor/Program.cs
+++ PongCliente_Sockets_GIT/PruebaServidor/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,58 @@ namespace PruebaServidor
     {
         static void Main(string[] args)
         {
+            // Indicamos nuestra ip local
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            // Establecemos el EndPoint con la dirección local y el puerto 80:
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 80);
+            // Create a TCP/IP socket.
+            Socket servidor = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // Asocia el endPoint indicado (ip y puerto) al socket:
+                servidor.Bind(localEndPoint);
+                // Establece la longitud máxima de la cola de conexiones
+                // pendientes que puede tener el servidor antes de empezar a rechazar conexiones:
+                servidor.Listen(10);
+                Console.WriteLine("Waiting for a connection..." + Environment.NewLine);
+                // Comienza a escuchar para recibir conexiones entrantes:
+                while (true)
+                {
+                    // El programa se suspende mientras está esperando la entrada de una conexión:
+                    Socket conexionRemota = servidor.Accept();
+                    if(conexionRemota.Connected) Console.WriteLine("Connected ");
+                    //AtenderPeticion(conexionRemota);
+                    Task.Run(() => AtenderPeticion(conexionRemota));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private static void AtenderPeticion(Socket conexionRemota)
+        {
+            // Data buffer para almacenar los datos recibidos
+            Byte[] bytes = new Byte[1024];
+            string datos = null;
+            int bytesRec = 0;
+
+            // Recepción de los datos:
+            bytesRec = conexionRemota.Receive(bytes);
+            datos += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            // Comprobar condición de parada de procesado de la información:
+
+            // Mostramos el texto recibido:
+            Console.WriteLine("Text received: " + Environment.NewLine + datos);
+            // Mensaje que enviaremos de respuesta al cliente:
+            Byte[] msg = Encoding.ASCII.GetBytes("Petición Recibida Correctamente");
+            // Envío de mensaje de respuesta al cliente:
+            conexionRemota.Send(msg);
+            // Apagado del socket:
+            conexionRemota.Shutdown(SocketShutdown.Both);
+            // Cierre y libera los recursos del socket:
+            conexionRemota.Close();
         }
     }
 }
--- PongCliente_Sockets_GIT/pruebaCliente/Program.cs
+++ PongCliente_Sockets_GIT/pruebaCliente/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace pruebaCliente
@@ -10,6 +13,49 @@ namespace pruebaCliente
     {
         static void Main(string[] args)
         {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 80);
+
+            Socket cliente = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            Byte[] msg = Encoding.ASCII.GetBytes("Me llamo cliente y este es un mensaje de prueba: 001");
+
+            try
+            {
+                cliente.Connect(localEndPoint);
+                if (cliente.Connected) Console.WriteLine("Connected ");
+
+                //Thread.Sleep(5000);
+
+                cliente.Send(msg);
+                Console.WriteLine("Msg sent ");
+
+                
+                recibirRespuesta(cliente);
+
+                Console.ReadKey(true);
+                //cliente.Shutdown(SocketShutdown.Both);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+
+        private static void recibirRespuesta(Socket conexionRemota)
+        {
+            Console.WriteLine("Waiting for response... ");
+            Byte[] recibedBytes = new Byte[1024];
+            int n_bytes = 0;
+            string respuesta = "";
+
+            n_bytes = conexionRemota.Receive(recibedBytes, recibedBytes.Length, SocketFlags.None);
+
+            respuesta = Encoding.ASCII.GetString(recibedBytes, 0, n_bytes);
+
+            Console.WriteLine(respuesta);
+
         }
     }
 }
