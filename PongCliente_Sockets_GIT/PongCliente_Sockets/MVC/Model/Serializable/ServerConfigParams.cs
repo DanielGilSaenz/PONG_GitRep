aae7e59704a5dcd27cc15077568dff3966bacab4
daniel <daniel.gil.saenz.trabajo@gmail.com>
2020-02-02 16:14:22 +0100

    hj

---
+++ PongCliente_Sockets_GIT/PongCliente_Sockets/MVC/Model/Serializable/ServerConfigParams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongCliente_Sockets.MVC.Model.Serializable
+{
+    class ServerConfigParams
+    {
+        public ServerConfigParams(string iP, Mode mode)
+        {
+            IP = iP;
+            this.mode = mode;
+        }
+
+        public enum Mode
+        {
+            ONLINE, OFFLINE
+        }
+
+        public string IP { get; set; }
+        public Mode mode { get; set; } = Mode.OFFLINE;
+    }
+}
