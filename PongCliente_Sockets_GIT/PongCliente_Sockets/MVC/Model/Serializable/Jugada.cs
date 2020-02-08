9b852aa03aafd663cb37240571ad534e2390c7e5
daniel <daniel.gil.saenz.trabajo@gmail.com>
2020-02-08 18:10:20 +0100

    Un fichero que no se habia stageado

---
+++ PongCliente_Sockets_GIT/PongCliente_Sockets/MVC/Model/Serializable/Jugada.cs
@@ -0,0 +1,25 @@
+﻿using PongCliente_Sockets.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongCliente_Sockets.MVC.Model.Serializable
+{
+    class Jugada : Mostrar
+    {
+        public Jugada()
+        {
+        }
+
+        public Jugada(Player player1, Ball ball)
+        {
+            this.player1 = player1;
+            this.ball = ball;
+        }
+
+        public Player player1 { get; set; }
+        public Ball ball { get; set; }
+    }
+}
